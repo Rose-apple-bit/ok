7dc7055510f49e4baa64b0883ebc1b28f070a017
Rose-apple-bit <111387565+Rose-apple-bit@users.noreply.github.com>
2022-08-17 11:17:19 -0700

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,41 @@
+import '../../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider } = configureChains(
+  [chain.polygon],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My Web3 App',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider coolMode chains={chains}>
+        <Head>
+          <title>Welcome to Web3!</title>
+        </Head>
+        <Toaster position="bottom-left" />
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default MyApp
